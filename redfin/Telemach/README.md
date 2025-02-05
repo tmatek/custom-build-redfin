04fd867f3286d20467f3972920e4eb28a588d9b1
Tadej Matek <matektadej@gmail.com>
2025-02-05 19:22:54 +0100

    add build config and some modem extensions.

---
+++ redfin/Telemach/README.md
@@ -0,0 +1,3 @@
+### Telemach (SI) modem config for VoLTE/5G
+
+Taken from OnePlus Pro 9 Android 14 firmware image (A1 Slovenia operator config).
---
+++ redfin/redfin-vendor.mk
@@ -0,0 +1,6 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/google/redfin
+
+PRODUCT_COPY_FILES += \
+    Telemach/mcfg_sw.mbn:$(TARGET_COPY_OUT_VENDOR)/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/generic/Pixel/EU/Telemach/Commercial/pixel_Slovenia/mcfg_sw.mbn \
+    mbn_sw.txt:$(TARGET_COPY_OUT_VENDOR)/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/mbn_sw.txt
