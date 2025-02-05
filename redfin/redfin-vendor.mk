29d01344f5c935f99a362d955a7bb8188446204e
Tadej Matek <matektadej@gmail.com>
2025-02-05 19:40:01 +0100

    switch to system properties.

--- redfin/redfin-vendor.mk
+++ redfin/redfin-vendor.mk
@@ -5,6 +5,6 @@ PRODUCT_COPY_FILES += \
     Telemach/mcfg_sw.mbn:$(TARGET_COPY_OUT_VENDOR)/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/generic/Pixel/EU/Telemach/Commercial/pixel_Slovenia/mcfg_sw.mbn \
     mbn_sw.txt:$(TARGET_COPY_OUT_VENDOR)/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/mbn_sw.txt
 
-PRODUCT_PRODUCT_PROPERTIES +=\
+PRODUCT_SYSTEM_PROPERTIES +=\
     persist.dbg.ims_volte_enable=1 \
     persist.dbg.volte_avail_ovr=1
