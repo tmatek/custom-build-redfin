ce2e0190e38d41bf89344c7e36995b9a9c21d622
Tadej Matek <matektadej@gmail.com>
2025-02-06 21:21:41 +0100

    add pre-build script.

---
+++ scripts/pre-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# copy custom modem config to proprietary blobs directory
+mkdir -p /srv/src/LINEAGE_18_1/vendor/google/redfin/proprietary/vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/generic/Pixel/EU/Telemach/Commercial/pixel_Slovenia
+cp /root/userscripts/Telemach/mcfg_sw.mbn $_
+echo "mcfg_sw/generic/Pixel/EU/Telemach/Commercial/pixel_Slovenia/mcfg_sw.mbn" >> /srv/src/LINEAGE_18_1/vendor/google/redfin/proprietary/vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/mbn_sw.txt
+
+# enable VoLTE for Telemach
+cp /root/userscripts/Telemach/carrier_config_carrierid_1712_Tusmobil-doo.xml /srv/src/LINEAGE_18_1/packages/apps/CarrierConfig/assets/carrier_config_carrierid_1712_Tusmobil-doo.xml
