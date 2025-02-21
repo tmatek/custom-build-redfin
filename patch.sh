a371e74c08d6d73b3777f63e6d64bc8b5debf83d
Tadej Matek <matektadej@gmail.com>
2025-02-21 17:19:18 +0100

    scripting.

--- patch.sh
+++ patch.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# https://github.com/PixelExperience/build/commit/8d0487a778a78a37866aa71d4305ebbc97ae8861
+git -C $AOSP_DIR/build/tools restore releasetools/sign_target_files_apks.py
+git -C $AOSP_DIR/build/tools apply ./patches/patch_sign_target_files_apks.diff
--- sign-build.sh
+++ sign-build.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+rm -rf $KEYS_DIR
+mkdir $KEYS_DIR
+
+subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+for x in releasekey platform shared media networkstack
+do
+  $AOSP_DIR/development/tools/make_key $KEYS_DIR/$x "$subject"
+done
