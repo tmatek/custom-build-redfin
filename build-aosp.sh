5c70ded5d6b171c5117302a9911e7acb62a35d53
Tadej Matek <matektadej@gmail.com>
2025-02-21 17:23:03 +0100

    info print.

--- build-aosp.sh
+++ build-aosp.sh
@@ -5,3 +5,5 @@ source $AOSP_DIR/build/envsetup.sh
 (cd $AOSP_DIR && lunch aosp_redfin-user)
 
 (cd $AOSP_DIR && make target-files-package otatools > $LOGS_DIR/build-aosp.txt 2>&1 &)
+
+echo "Build started. Logs in $LOGS_DIR/build-aosp.txt"
