e1bb38e11064124d9fecb00bab99f2b9dd602a8a
Tadej Matek <matektadej@gmail.com>
2025-02-24 17:03:41 +0100

    fix typo.

--- sign-build.sh
+++ sign-build.sh
@@ -13,6 +13,6 @@ done
 openssl pkcs8 -in $KEYS_DIR/releasekey.pk8 -inform DER -out $KEYS_DIR/releasekey.key -nocrypt
 
 (cd $AOSP_DIR && ./out/host/linux_/bin/sign_target_files_apks -o --default_key_mappings $KEYS_DIR \
-  ./out/target/product/redfin/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip ./out/signed-target_files.zip > $LOGS_DIR/sign-build.txt 2>&1 &)
+  ./out/target/product/redfin/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip ./out/signed-target-files.zip > $LOGS_DIR/sign-build.txt 2>&1 &)
 
 echo "Signing started. Logs in $LOGS_DIR/sign-build.txt"
