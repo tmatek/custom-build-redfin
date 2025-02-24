42848bbbc353305e5214f483325c643c7be6d8dd
Tadej Matek <matektadej@gmail.com>
2025-02-24 15:27:06 +0100

    minor fix.

--- build-aosp.sh
+++ build-aosp.sh
@@ -2,6 +2,8 @@
 
 cp -a $KERNEL_DIR/out/android-msm-pixel-4.19/dist/. $AOSP_DIR/device/google/redbull/
 
-(cd $AOSP_DIR && source build/envsetup.sh && lunch aosp_redfin-user && make target-files-package otatools > $LOGS_DIR/build-aosp.txt 2>&1 &)
+source $AOSP_DIR/build/envsetup.sh
+
+(cd $AOSP_DIR && lunch aosp_redfin-user && make target-files-package otatools > $LOGS_DIR/build-aosp.txt 2>&1 &)
 
 echo "Build started. Logs in $LOGS_DIR/build-aosp.txt"
