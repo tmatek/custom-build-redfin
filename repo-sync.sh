e9258d8939582d363426e2e9d64023683e27ed5e
Tadej Matek <matektadej@gmail.com>
2025-02-28 20:12:53 +0100

    use temp dir for driver binaries.

--- repo-sync.sh
+++ repo-sync.sh
@@ -5,15 +5,14 @@
 (cd $AOSP_DIR && repo sync -c)
 
 # driver binaries
-curl https://dl.google.com/dl/android/aosp/google_devices-redfin-rq3a.211001.001-bd720345.tgz > google_redfin.tgz
-curl https://dl.google.com/dl/android/aosp/qcom-redfin-rq3a.211001.001-71f56923.tgz > qcom_redfin.tgz
+DRIVER_BASE=$(mktemp -d)
+curl https://dl.google.com/dl/android/aosp/google_devices-redfin-rq3a.211001.001-bd720345.tgz > $DRIVER_BASE/google_redfin.tgz
+curl https://dl.google.com/dl/android/aosp/qcom-redfin-rq3a.211001.001-71f56923.tgz > $DRIVER_BASE/qcom_redfin.tgz
 
-tar -xzvf google_redfin.tgz && rm google_redfin.tgz
-mv extract-google_devices-redfin.sh $AOSP_DIR
+tar -xzvf $DRIVER_BASE/google_redfin.tgz -C $AOSP_DIR
 (cd $AOSP_DIR && ./extract-google_devices-redfin.sh)
 
-tar -xzvf qcom_redfin.tgz && rm qcom_redfin.tgz
-mv extract-qcom-redfin.sh $AOSP_DIR
+tar -xzvf $DRIVER_BASE/qcom_redfin.tgz -C $AOSP_DIR
 (cd $AOSP_DIR && ./extract-qcom-redfin.sh)
 
 # kernel
